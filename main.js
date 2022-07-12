5d41a74691489df8c170a01a6af841fdb0d1bb45
mirmahmud2007 <mirmahmudmirsali@gmail.com>
2022-07-12 14:35:04 +0500

    kilometr

---
+++ main.js
@@ -0,0 +1,6 @@
+function moneyConverter(valNum) {
+    document.getElementById("outputrub").innerHTML=valNum*16.66;
+    document.getElementById("outputrub1").innerHTML=valNum*2.98;
+    document.getElementById("outputrub2").innerHTML=valNum*0.8;
+    document.getElementById("outputrub3").innerHTML=valNum*0.075;
+}
